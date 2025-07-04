81866e3bc04da6701ece555dc9460169a2a0b2af
Mihrac199 <mihrac.comart1996@gmail.com>
2025-07-04 15:08:07 +0300

    İnitial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# World Map Project (React)
+
+We can perform operations at any location we want on the world map.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+
+  return (
+
+    <div>
+      <h1>WORLDWİSE</h1>
+    </div>
+
+  )
+
+}
